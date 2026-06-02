e1a887a7d2424eba299934b1fffccc2ba63eb804
Nazmul Hassan <nazmulhassanlabid@gmail.com>
2026-06-02 16:13:40 +0600

    Implement dinosaur jumping mechanics and images

--- Chrome Dinosaur Game/ChromeDinosaur/src/ChromeDinosaur.java
+++ Chrome Dinosaur Game/ChromeDinosaur/src/ChromeDinosaur.java
@@ -3,12 +3,107 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-public class ChromeDinosaur extends JPanel {
-    private int boardWidth = 850;
-    private int boardHeight = 250;
+public class ChromeDinosaur extends JPanel implements ActionListener, KeyListener{
+    int boardWidth = 850;
+    int boardHeight = 250;
+
+    // Load Image
+    Image dinosaurImg;
+    Image dinosaurDeadImg;
+    Image dinosaurJumpImg;
+    Image cactus1Img;
+    Image cactus2Img;
+    Image cactus3Img;
+
+    class Block{
+        int x;
+        int y;
+        int width;
+        int height;
+        Image img;
+
+        Block(int x, int y, int width, int height, Image img){
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
+    }
+
+    int dinosaurWidth = 80;
+    int dinosaurHeight = 90;
+    int dinosaurX = 50;
+    int dinosaurY = boardHeight - dinosaurHeight;
+
+    Block dinosaur;
+
+    int velocityY = 0; // Dinosaur jumping speed
+    int gravity = 1;
+
+    Timer gameLoop;
 
     public ChromeDinosaur(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.lightGray);
+        setFocusable(true);
+        addKeyListener(this);
+
+        dinosaurImg = new ImageIcon(getClass().getResource("./img/dino-run.gif")).getImage();
+        dinosaurDeadImg = new ImageIcon(getClass().getResource("./img/dino-dead.png")).getImage();
+        dinosaurJumpImg = new ImageIcon(getClass().getResource("./img/dino-jump.png")).getImage();
+        cactus1Img = new ImageIcon(getClass().getResource("./img/cactus1.png")).getImage();
+        cactus2Img = new ImageIcon(getClass().getResource("./img/cactus2.png")).getImage();
+        cactus3Img = new ImageIcon(getClass().getResource("./img/cactus3.png")).getImage();
+
+        dinosaur = new Block(dinosaurX, dinosaurY, dinosaurWidth, dinosaurHeight, dinosaurImg);
+
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
     }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        g.drawImage(dinosaur.img, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null);
+    }
+
+
+    public void move(){
+        velocityY += gravity;
+        dinosaur.y += velocityY;
+
+        if(dinosaur.y > dinosaurY){
+            dinosaur.y = dinosaurY;
+            velocityY = 0;
+            dinosaur.img = dinosaurImg;
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) { 
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            if(dinosaur.y == dinosaurY){
+            velocityY = -17;
+            dinosaur.img = dinosaurJumpImg;
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) { }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
 }
