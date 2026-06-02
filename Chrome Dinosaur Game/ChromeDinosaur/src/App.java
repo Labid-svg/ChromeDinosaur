218da940a6868389e7d38016e1056c82efcf453c
Nazmul Hassan <nazmulhassanlabid@gmail.com>
2026-06-02 16:07:13 +0600

    Modify App class for game window and component setup
    Updated the game window dimensions and added the ChromeDinosaur component to the frame.

--- Chrome Dinosaur Game/ChromeDinosaur/src/App.java
+++ Chrome Dinosaur Game/ChromeDinosaur/src/App.java
@@ -2,15 +2,20 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int boardWidth = 850;
-        int boardHeight = 350;
+        int boardWidth = 750;
+        int boardHeight = 150;
 
-        JFrame frame = new JFrame("Chrome Dinosaur");
+        JFrame frame = new JFrame("Dinosaur");
+        // frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
-        frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
+        ChromeDinosaur chromeDinosaur = new ChromeDinosaur();
+        frame.add(chromeDinosaur);
+        frame.pack();
+        chromeDinosaur.requestFocus();
+        frame.setVisible(true);
     }
 }
